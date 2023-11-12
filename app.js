e7aa13d47d816422cfe87ebe2c599dabe7e80eb8
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-11-12 18:51:36 +0700

    feat: setup docker and connect to db

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const express = require('express');
-const app = express();
+const bcrypt = require("bcrypt");
+const express = require("express");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const { sequelize, User, TodoList, TodoItem } = require("./models");
+
+sequelize.sync({ force: true }).then(() => {
+  console.log("Database synchronized");
+});
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
