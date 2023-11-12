7205bbc1b928025f0252bf9292e763191f3a91e3
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-11-12 21:30:32 +0700

    feat: implement create list item

--- app.js
+++ app.js
@@ -52,6 +52,25 @@ const canAccessTodoList = async (req, res, next) => {
   }
 };
 
+const canEditTodoList = async (req, res, next) => {
+  const uid = req.session.uid;
+  const listId = req.params.listId;
+
+  try {
+    const permission = await UserTodo.findOne({
+      where: { user_id: uid, list_id: listId },
+    });
+
+    if (permission && permission.access_type === "edit") {
+      return next();
+    } else {
+      return res.status(403).json({ error: "Not authorized" });
+    }
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
+  }
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -131,37 +150,15 @@ app.get("/lists", isAuthenticated, async (req, res) => {
 
     if (lists && lists.length) {
       return res.json(lists);
-    } else {
-      return res
-        .status(404)
-        .json({ error: "No to-do lists found for this user" });
     }
+
+    return res
+      .status(404)
+      .json({ error: `No to-do lists found for user with id ${uid}` });
   } catch (e) {
     return res.status(500).json({ error: e.message });
   }
 });
-
-app.get(
-  "/lists/:listId",
-  isAuthenticated,
-  canAccessTodoList,
-  async (req, res) => {
-    const listId = req.params.uid;
-
-    try {
-      const items = await TodoItem.findAll({ where: { list_id: listId } });
-      if (items && items.length) {
-        return res.json(items);
-      } else {
-        return res
-          .status(404)
-          .json({ message: "No to-do items found for this list" });
-      }
-    } catch (e) {
-      return res.status(500).json({ error: e.message });
-    }
-  }
-);
 
 app.post("/lists", isAuthenticated, async (req, res) => {
   const uid = req.session.uid;
@@ -173,6 +170,7 @@ app.post("/lists", isAuthenticated, async (req, res) => {
 
   try {
     const transaction = await sequelize.transaction();
+
     const list = await TodoList.create(
       { title, description, owner_id: uid },
       { transaction }
@@ -181,6 +179,7 @@ app.post("/lists", isAuthenticated, async (req, res) => {
       { user_id: uid, list_id: list.id, access_type: "edit" },
       { transaction }
     );
+
     await transaction.commit();
 
     return res
@@ -190,6 +189,87 @@ app.post("/lists", isAuthenticated, async (req, res) => {
     return res.status(500).json({ error: e.message });
   }
 });
+
+app.get(
+  "/lists/:listId/items",
+  isAuthenticated,
+  canAccessTodoList,
+  async (req, res) => {
+    const listId = req.params.listId;
+
+    try {
+      const list = await TodoList.findOne({
+        where: { id: listId },
+        include: [{ model: TodoItem }],
+      });
+
+      if (!list) {
+        return res
+          .status(404)
+          .json({ message: `No to-do list found for id ${listId}` });
+      }
+
+      const items = await TodoItem.findAll({ where: { list_id: listId } });
+
+      if (items && items.length) {
+        return res.json(items);
+      }
+
+      return res
+        .status(404)
+        .json({ message: `No to-do items found for list with id ${listId}` });
+    } catch (e) {
+      return res.status(500).json({ error: e.message });
+    }
+  }
+);
+
+app.post(
+  "/lists/:listId/items",
+  isAuthenticated,
+  canEditTodoList,
+  async (req, res) => {
+    const listId = req.params.listId;
+    const { content, due_date } = req.body;
+
+    if (!content || !due_date) {
+      return res.status(400).json({ error: "Missing required body params" });
+    }
+
+    try {
+      const list = await TodoList.findOne({
+        where: { id: listId },
+        include: [{ model: TodoItem }],
+      });
+
+      if (!list) {
+        return res
+          .status(404)
+          .json({ message: `No to-do list found for id ${listId}` });
+      }
+
+      await TodoItem.create({
+        content,
+        due_date,
+        list_id: listId,
+      });
+
+      const updatedList = await TodoList.findOne({
+        where: { id: listId },
+        include: [{ model: TodoItem }],
+      });
+
+      return res
+        .status(201)
+        .json({
+          message: "To-do item created successfully",
+          list: updatedList,
+        });
+    } catch (e) {
+      return res.status(500).json({ error: e.message });
+    }
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
